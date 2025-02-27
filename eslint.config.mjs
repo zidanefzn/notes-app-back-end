905f32c85be4ac7a45afa4ee357aaa376c67addb
zidanefzn <zidan4164@gmail.com>
2025-02-27 16:44:54 +0700

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import daStyle from 'eslint-config-dicodingacademy';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  daStyle, {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
+  {languageOptions: { globals:globals.node }},
+  pluginJs.configs.recommended,
+];
